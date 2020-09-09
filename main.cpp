c68774415725c7b331f2f40e48175aa88ed18cdd
dlclose <54722623+dlclose@users.noreply.github.com>
2020-09-09 13:16:58 -0700

    printed out list

--- main.cpp
+++ main.cpp
@@ -25,6 +25,15 @@ int main()
         list.push_back(item);
     }
   }while( input != 'q' && input != 'Q' );
+  if((int)list.size() == 0){
+    cout << "there are no items on the list."
+  }
+  else{
+    for(int i = 0; i < (int)list.size(); i++){
+      cout << i << " " << list[i] << endl;
+    }
+  }
+
     
   return 0;
 }
